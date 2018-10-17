ec8ad5d53e1c7b9b066acb16db735f295fcf78bf
Bob Jeltes <bobjeltes@hotmail.com>
2018-10-17 21:44:49 +0200

    Small test for code review

---
+++ Assets/Code/DaanTestScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DaanTestScript : MonoBehaviour {
+
+    public Rigidbody rigBod;
+
+	// Use this for initialization
+	void Start () {
+
+        rigBod = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
